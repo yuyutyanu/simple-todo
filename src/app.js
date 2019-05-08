5d896920ff32dc37fb2571fc497619f581c81137
chanu <yuyutyanu@yahoo.co.jp>
2019-05-09 00:50:50 +0900

    add: todo

--- src/app.js
+++ src/app.js
@@ -5,17 +5,48 @@ import About from './pages/about'
 import Friend from './pages/friend'
 import Layout from './layout/layout'
 
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+import TodoList from './components/TodoList'
+import Footer from './components/Footer'
+
+// <Router>
+// <Layout>
+// <Switch>
+// <Route exact path="/" component={Home}/>
+// <Route path="/about" component={About}/>
+// <Route path="/friend" component={Friend}/>
+// </Switch>
+// </Layout>
+// </Router>
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/friend" component={Friend}/>
-      </Switch>
-    </Layout>
-  </Router>
+  <div>
+    <AddTodo onAddClick={text => {console.log(text)}}/>
+    <TodoList todos={
+      [
+        {
+          text: 'Use Redux',
+          completed: true
+        },
+        {
+          text: 'Learn to connect it to React',
+          completed: false
+        }
+      ]
+    } onTodoClick={index =>
+      console.log('todo clicked', index)
+    }
+    />
+    <Footer
+      filter='SHOW_ALL'
+      onFilterChange={
+        filter => {
+          console.log('filter change', filter)
+        }
+      }
+    />
+  </div>
 )
 
 export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export default class Footer extends Component{
+  renderFilter(filter, name){
+    if(filter === this.props.filter){
+      return name
+    }
+
+    return (
+      <a href="#" onClick={e =>{
+        e.preventDefault()
+        this.props.onFilterChange(filter)
+      }}>
+        {name}
+      </a>
+    )
+  }
+  render () {
+    return (
+      <p>
+        Show:
+        {' '}
+        {this.renderFilter('SHOW_ALL', 'ALL')}
+        {','}
+        {this.renderFilter('SHOW_COMPLETE', 'COMPLETE')}
+        {','}
+        {this.renderFilter('SHOW_ACTIVE', 'ACTIVE')}
+        .
+      </p>
+    );
+  }
+}
+
+Footer.propTypes = {
+  onFilterChange: PropTypes.func.isRequired,
+  filter: PropTypes.oneOf([
+    'SHOW_ALL',
+    'SHOW_COMPLETE',
+    'SHOW_ACTIVE'
+    ]).isRequired
+}
