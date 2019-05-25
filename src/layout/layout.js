f2074610d0b5ce66489d6d1d8ff9b08f3597136c
chanu <yuyutyanu@yahoo.co.jp>
2019-05-25 20:24:32 +0900

    update

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './header'
-import Footer from './footer'
 
 const Layout = (props) =>(
   <div>
     <Header/>
     {props.children}
-    {/*<Footer/>*/}
   </div>
 )
 
